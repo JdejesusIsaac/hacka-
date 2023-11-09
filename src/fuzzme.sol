5c72bcf9dbaeac8815047eb73afe1ce5dd42e83d
Angelica Colon <juandejesusisaac@gmail.com>
2023-11-09 08:57:10 -0500

    new

--- src/fuzzme.sol
+++ src/fuzzme.sol
@@ -15,9 +15,16 @@ contract Vesting is
     
 {
     using SafeERC20 for IERC20;
+   
     IERC20 public projectToken;
     
     address public padContract;
+
+    uint256 public vestingStartTime; // Vesting period start time
+
+    uint256 public LOW_FDV_VESTING_DURATION; // Vesting period for Low FDV Vesting Phase
+    uint256 public HIGH_FDV_VESTING_DURATION; // Vesting period for Public Vesting Phase
+
 
     
 
@@ -33,7 +40,9 @@ contract Vesting is
     event VestedTokens(address indexed user, bool isLowFDVedPhase, uint256 amount);
     event padSet(address newLaunchpadAddress);
 
-     error Onlypad();
+    event VestingStartTimeSet(uint256 newStartTime);
+
+    error Onlypad();
 
     error AddressZero();
 
@@ -58,6 +67,75 @@ contract Vesting is
         emit padSet(_newAddress);
     }
 
+     function configLaunchpadVesting(
+        address _projectToken,
+        uint256 _lowFDVVestingDuration,
+        uint256 _highFDVVestingDuration
+    ) external onlyOwner {
+        if (address(_projectToken) == address(0)) revert AddressZero();
+        projectToken = IERC20(_projectToken);
+
+        LOW_FDV_VESTING_DURATION = _lowFDVVestingDuration;
+        HIGH_FDV_VESTING_DURATION = _highFDVVestingDuration;
+    }
+
+    function getClaimable(
+        address account
+    ) external view returns (uint256 lowFDVAmount, uint256 highFDVAmount) {
+        VestingInfo storage vestingData = vestingInfo[account];
+        uint256 currentTime = _currentBlockTimestamp();
+
+        if (
+            vestingData.lowFDVAmount != 0 &&
+            vestingData.lowFDVAmount > vestingData.lowFDVClaimedAmount
+        ) {
+            uint256 endTime = vestingStartTime + LOW_FDV_VESTING_DURATION;
+            if (currentTime < endTime) {
+                lowFDVAmount =
+                    ((currentTime - vestingStartTime) * vestingData.lowFDVAmount) /
+                    LOW_FDV_VESTING_DURATION;
+            } else {
+                lowFDVAmount = vestingData.lowFDVAmount;
+            }
+            lowFDVAmount -= vestingData.lowFDVClaimedAmount;
+        }
+
+        if (
+            vestingData.highFDVAmount != 0 &&
+            vestingData.highFDVAmount > vestingData.highFDVClaimedAmount
+        ) {
+            uint256 endTime = vestingStartTime + HIGH_FDV_VESTING_DURATION;
+            if (currentTime < endTime) {
+                highFDVAmount =
+                    ((currentTime - vestingStartTime) * vestingData.highFDVAmount) /
+                    HIGH_FDV_VESTING_DURATION;
+            } else {
+                highFDVAmount = vestingData.highFDVAmount;
+            }
+            highFDVAmount -= vestingData.highFDVClaimedAmount;
+        }
+    }
+
+    /// @dev claim vested tokens
+    function claim() external nonReentrant {
+        (uint256 lowFDVAmount, uint256 highFDVAmount) = this.getClaimable(msg.sender);
+
+        if (lowFDVAmount + highFDVAmount > 0) {
+            VestingInfo storage vestingData = vestingInfo[msg.sender];
+
+            vestingData.lowFDVClaimedAmount = vestingData.lowFDVClaimedAmount + lowFDVAmount;
+            vestingData.highFDVClaimedAmount = vestingData.highFDVClaimedAmount + highFDVAmount;
+            projectToken.safeTransfer(msg.sender, lowFDVAmount + highFDVAmount);
+        }
+    }
+
+    /// @dev Setting start time for vesting period
+    function setVestingStartTime(uint256 _startTime) external onlypad {
+        vestingStartTime = _startTime;
+        emit VestingStartTimeSet(_startTime);
+    }
+
+
     function vestTokens(
         bool isLowFDVedVesting,
         uint256 amount,
@@ -69,8 +147,8 @@ contract Vesting is
         emit VestedTokens(vestFor, isLowFDVedVesting, amount);
     }
 
-
-function _processVesting(bool isLowFDVedVesting, uint256 amount, address vestFor) internal {
+    
+    function _processVesting(bool isLowFDVedVesting, uint256 amount, address vestFor) internal {
         if (isLowFDVedVesting) {
             vestingInfo[vestFor].lowFDVAmount = amount;
         } else {
@@ -78,16 +156,17 @@ function _processVesting(bool isLowFDVedVesting, uint256 amount, address vestFor
         }
     }
 
-
-   
-
-
-   
-
-   
-   
-
+     function _currentBlockTimestamp() internal view returns (uint256) {
+        return block.timestamp;
+    }
 }
+
+
+
+
+
+
+
 
 
     
